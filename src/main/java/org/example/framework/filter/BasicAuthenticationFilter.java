34864870347c4d6edc77eb84c28425e928dde897
Igor Khristiuk <ikhristiuk@gmail.com>
2021-10-03 09:54:45 +0300

    fixed #2

--- src/main/java/org/example/framework/filter/BasicAuthenticationFilter.java
+++ src/main/java/org/example/framework/filter/BasicAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.app.exception.WrongLoginAndPassException;
 import org.example.framework.attribute.ContextAttributes;
 import org.example.framework.attribute.RequestAttributes;
 import org.example.framework.security.*;
@@ -14,6 +15,7 @@ import org.example.framework.util.FilterHelper;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.regex.Pattern;
 
 public class BasicAuthenticationFilter extends HttpFilter {
@@ -39,8 +41,23 @@ public class BasicAuthenticationFilter extends HttpFilter {
             return;
         }
 
+        final var schemaAndEncodeData = auth.split(" ");
+        if (schemaAndEncodeData.length != 2) {
+            throw new WrongLoginAndPassException();
+        }
+
+        final var loginAndPass = new String(Base64.getDecoder().decode(schemaAndEncodeData[1]));
+        final var split = loginAndPass.split(":");
+        if (split.length != 2) {
+            throw new WrongLoginAndPassException();
+        }
+
+        final var principal = split[0];
+        final var credentials = split[1];
+
         try {
-            final var authentication = provider.authenticate(new BasicAuthentication(auth, null));
+            final var authentication = provider.authenticate
+                    (new BasicAuthentication(principal, credentials));
             req.setAttribute(RequestAttributes.AUTH_ATTR, authentication);
         } catch (AuthenticationException e) {
             res.sendError(401);
